4f58022176a4d0be5856d38402f1125b59e4bb72
santmend <90207999+santmend@users.noreply.github.com>
2021-09-20 17:14:44 -0500

    Primer commit

---
+++ src/main/java/Numero.java
@@ -0,0 +1,14 @@
+public abstract class Numero {
+
+        public static void main(String []args){
+     
+        operaciones operaciones = new operaciones();
+        
+        System.out.println("Suma = " + operaciones.suma(10, 5));
+        System.out.println("Resta = " + operaciones.resta(10, 5));
+        System.out.println("Multiplicacion = " + operaciones.multiplicacion(10, 5));
+        System.out.println("Division = " + operaciones.division(10, 5));
+          
+        }    
+}
+    
---
+++ src/main/java/operaciones.java
@@ -0,0 +1,23 @@
+ public class operaciones extends Numero{
+    int numero=10;
+    public int suma(int a, int b) {
+        int name = this.numero + a + b;
+        return (numero+ a + b);
+    }
+    public int resta(int a, int b) {
+        int name =this.numero - a - b;
+         return (numero - a - b);
+    }
+     public int multiplicacion(int a, int b) {
+         int name = this.numero * a * b;
+         return (numero * a * b);
+    }
+     public int division(int a, int b) {
+         int name = this.numero / a / b;
+         return (numero / a / b); 
+   }
+}
+ 
+         
+         
+
